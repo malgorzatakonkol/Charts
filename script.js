855060b1793b594ab44740e433a5b7cf2bf89a4d
Małgorzata Konkol <kalafar.malgorzata@gmail.com>
2018-06-15 15:26:29 +0200

    firs commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var chart = new Highcharts.Chart({
+var chartHorizontal = new Highcharts.Chart({
     chart: {
         renderTo: 'container',
         type: 'column'
@@ -8,6 +8,18 @@ var chart = new Highcharts.Chart({
     },
     subtitle: {
         text: 'What area of IT do you work in?'
+    },
+
+    legend: {
+        enabled: false
+    },
+
+    plotOptions: {
+        column: {
+            dataLabels: {
+                enabled: true
+            }
+        }
     },
     xAxis:
         {
@@ -33,13 +45,74 @@ var chart = new Highcharts.Chart({
 
 
 $('#small').click(function () {
-    chart.setSize(400);
+    chartHorizontal.setSize(400);
 });
 
 $('#large').click(function () {
-    chart.setSize(800);
+    chartHorizontal.setSize(800);
 });
 
 $('#auto').click(function () {
-    chart.setSize(null);
+    chartHorizontal.setSize(null);
+});
+
+Highcharts.chart ('container1',{
+    chart: {
+        type: 'bar'
+    },
+    title: {
+        text: 'SALARY LEVEL'
+    },
+    subtitle: {
+        text: 'What is your average monthly net salary (take-home salary)?'
+    },
+    xAxis: {
+        type: 'category',
+        title: {
+            text: null
+        },
+        min: 0,
+        max: 4,
+        scrollbar: {
+            enabled: true
+        },
+        tickLength: 0
+    },
+    yAxis: {
+        min: 0,
+        max: 25,
+        title: {
+            text: 'Data in %',
+            align: 'high'
+        }
+    },
+    plotOptions: {
+        bar: {
+            dataLabels: {
+                enabled: true
+            }
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    credits: {
+        enabled: false
+    },
+    series: [{
+        name: 'Employed in the IT industry',
+        data: [
+            ['Less than PLN 1500', 2.0],
+            ["PLN 1500-3000", 19.7],
+            ["PLN 3001-4000", 20.1],
+            ["PLN 4001-5000", 16.4],
+            ["PLN 5001-6000", 11.5],
+            ["PLN 6001-7000", 7.9],
+            ["PLN 7001-8000", 5.6],
+            ["PLN 8001-9000", 3.4],
+            ["PLN 9001-10000", 1.2],
+            ["PLN 10001-15000", 3.8],
+            ["More than 15 000", 0.9]
+        ]
+    }]
 });
